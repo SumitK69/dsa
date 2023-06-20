170be239fe6b04ab9cbbebad6024207b1b9938d4
sumit khobragade <sumitbk1010@gmail.com>
2023-06-20 20:59:57 +0530

    first non repeating  accepted

---
+++ maps/firstNonRepeating.cpp
@@ -0,0 +1,28 @@
+#include <bits/stdc++.h> 
+using namespace std;
+char firstNonRepeatingCharacter(string str) {
+    int f = 0;
+    for (int i = 0; i < str.size(); i++) {
+
+        f = 0;
+        for (int j = 0; j < str.size(); j++) {
+
+        if (str[i] == str[j] && i != j) {
+            f = 1;
+            break;
+        }
+
+        }
+
+        if (f == 0) {
+        return str[i];
+        }
+    }
+    return str[0];
+}
+
+int main(){
+    string str = "acabDbd";
+    cout<<firstNonRepeatingCharacter(str);
+    return 0;
+}
