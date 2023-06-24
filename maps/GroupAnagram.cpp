2b06379703a64b98a302b211d0ec6de8bb8e928e
sumit khobragade <sumitbk1010@gmail.com>
2023-06-24 23:51:23 +0530

    Group Anagram accepted

---
+++ maps/GroupAnagram.cpp
@@ -0,0 +1,38 @@
+#include <bits/stdc++.h> 
+using namespace std;
+vector<vector<string>> getGroupedAnagrams(vector<string> &inputStr, int n)
+{
+    vector<vector<string>> hope;
+    unordered_map<string,vector<string>> store;
+
+     for (int i = 0; i < inputStr.size(); i++)
+    {
+        string tempString(inputStr[i]);
+       
+        sort(tempString.begin(),tempString.end());
+       
+        store[tempString].push_back(inputStr[i]);
+    }
+  
+
+    for (auto it:store)
+    {
+        vector<string> temp_vec(it.second);
+        int size = temp_vec.size();
+   
+       
+        for (int i = 0; i < size; i++)
+        cout << temp_vec[i] << " ";
+           
+        cout << "\n";
+    }
+}
+
+int main(){
+    vector<string> inputStr = {"eat","tea","tan","ate","nat","bat"};
+    // vector<string> inputStr = {"abab", "baba", "aabb", "abbc"};
+    getGroupedAnagrams(inputStr,inputStr.size());
+    
+
+    return 0;
+}
